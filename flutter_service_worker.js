8a4d3c4c6fbcc53b41f4e69f5905987ba1c80511
singularity-s0 <12184989+singularity-s0@users.noreply.github.com>
2023-02-08 07:42:35 +0000

    Deploying to gh-pages from @ singularity-s0/openchat_frontend@28b6230c911a0d02bc02147524ea4a23f9b5c3ca 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"index.html": "9b91829b6f211c750f4825ff45a3577c",
-"/": "9b91829b6f211c750f4825ff45a3577c",
-"main.dart.js": "47b62d0719761bfe4a5f5b0fd95daab3",
+"index.html": "0aee5ce840a343f706275661ab6ef9c5",
+"/": "0aee5ce840a343f706275661ab6ef9c5",
+"main.dart.js": "3799649edb5f8b7cb7f84e29a31013a6",
 "manifest.json": "a2202aad6859603b2fbb6892536cca01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",

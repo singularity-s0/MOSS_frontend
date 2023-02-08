782c7ae7288efd86a0d7f6196cec9d6496e39e4d
singularity <12184989+singularity-s0@users.noreply.github.com>
2023-02-08 16:52:52 +0800

    fix asset

--- lib/views/components/chat_ui/widgets/input/send_button.dart
+++ lib/views/components/chat_ui/widgets/input/send_button.dart
@@ -28,11 +28,8 @@ class SendButton extends StatelessWidget {
             minWidth: 24,
           ),
           icon: InheritedChatTheme.of(context).theme.sendButtonIcon ??
-              Image.asset(
-                'assets/icon-send.png',
-                color: InheritedChatTheme.of(context).theme.inputTextColor,
-                package: 'flutter_chat_ui',
-              ),
+              Icon(Icons.send,
+                  color: InheritedChatTheme.of(context).theme.inputTextColor),
           onPressed: onPressed,
           padding: padding,
           splashRadius: 24,

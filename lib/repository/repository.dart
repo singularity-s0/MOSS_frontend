c9e28251c2e9c7e9eaec6408bb91d44c5424c659
singularity <12184989+singularity-s0@users.noreply.github.com>
2023-02-18 22:33:30 +0800

    update repository

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -10,8 +10,10 @@ class Repository {
 
   factory Repository.getInstance() => _instance;
 
+  // static const String baseUrl = "https://moss.jingyijun.xyz:12443/api";
+  // static const String wsBaseUrl = "ws://moss.jingyijun.xyz:12442/api/ws";
   static const String baseUrl = "https://moss.fastnlp.top/api";
-  static const String wsBaseUrl = "ws://moss.jingyijun.xyz:12442/api/ws";
+  static const String wsBaseUrl = "ws://moss.fastnlp.top/api/ws";
 
   static const String waitlistUrl = "https://survey.moss.fastnlp.top/s/Ntd4eG";
 
